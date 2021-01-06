0b72107d8d61e2bf18d2f89677a01b507c8a64ca
Arunava Banerjee <Arunava Banerjee@DESKTOP-RBAIMPE>
2021-01-06 05:32:55 -0800

    2 PEOPLE TRYING TO MANIPULATE THE SAME ACCOUNT CONCURRENTLY

--- AsynchronousBankAccount/Program.cs
+++ AsynchronousBankAccount/Program.cs
@@ -3,13 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Linq;
 
 namespace AsynchronousBankAccount
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            SampleDataContext db = new SampleDataContext();
+            JointAccount j =  db.JointAccounts.Where(x => x.id == 1).Single();
+
+          
+
+            try
+            {
+                // User 1 tries to deposit 500 to the account
+                await deposit(500, j, db);
+
+
+
+                // user 2 tries to withdraw 200 at the same time when user 1 is depositng
+                db.ExecuteCommand("Update JointAccount set balance = balance - 200 where id=1");
+
+
+
+                // change conflict exception occurs 
+                db.SubmitChanges();
+
+
+
+            }
+
+            catch(Exception e)
+            {
+
+                db.ChangeConflicts.ResolveAll(RefreshMode.KeepCurrentValues);  // preference will only be given to user 1 actions
+                db.SubmitChanges();
+
+            }
+
+            Console.WriteLine(j.balance);
+           
+
         }
+
+
+
+
+
+        public static async Task deposit(int amount , JointAccount j,SampleDataContext db)
+        {
+            j.balance += amount;    
+
+            await Task.Delay(2000);
+
+            
+
+            
+        }
+
+      
+
+
+
     }
 }
